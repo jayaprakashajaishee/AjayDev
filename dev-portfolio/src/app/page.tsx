205c2ee4c281ed0110d6cd82730085c279fe5fff
jayaprakashajaishee <jayaprakashajaishee@gmail.com>
2023-09-01 21:25:15 +0530

    initialize project

---
+++ dev-portfolio/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div style={{ height: "100vh", backgroundColor: "red" }}>1</div>
+      <div style={{ height: "100vh", backgroundColor: "yellow" }}>1</div>
+      <div style={{ height: "100vh", backgroundColor: "green" }}>1</div>
+    </>
+  );
+}
